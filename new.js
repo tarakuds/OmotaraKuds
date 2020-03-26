37e76a57e7dd8c253474cf90de7fe77f544cd547
tarakuds <37577346+tarakuds@users.noreply.github.com>
2020-03-26 22:59:28 +0100

    new js
    javascript

---
+++ new.js
@@ -0,0 +1,11 @@
+ var fullName = "Omotara Kudehinbu";
+ var courses = ['html', 'css', 'js', 'php', 'python','nodejs', 'C#'];
+ 
+ console.log(fullName);
+ console.log(courses);
+
+
+ for (var i =0; i<=220; i++)
+ 	{if (i % 2 ===1){
+ 		console.log(i) ;
+ 	}
